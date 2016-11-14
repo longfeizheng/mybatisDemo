eacb442efe02b5bfd732cd3e6394d10ec4a4156d
longfeizheng <longfei_zheng@163.com>
2016-11-14 21:53:21 +0800

    添加 mybatis Generate 自动生成 po mapper

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -12,3 +12,17 @@ CREATE TABLE `tb_user` (
   UNIQUE KEY `username` (`username`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
+CREATE TABLE `ta_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(32) NOT NULL COMMENT '密码，加密存储',
+  `name` varchar(50)  COMMENT '姓名',
+  `age` int  COMMENT '年龄',
+  `sex` int  COMMENT '性别',
+  `birthday` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
